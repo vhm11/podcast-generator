fddd2f41026fec98aa61e5e776a7252e64c5d36c
Valentina Herrera <137663407+vhm11@users.noreply.github.com>
2026-01-28 11:17:17 -0500

    Update email configuration and echo messages

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-echo "============="
+echo "==================="
 
 git config --global user.name "${GITHUB_ACTOR}"
-git config --global user.email "${GITHUB_EMAIL}"
+git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
 python3 /usr/bin/feed.py
 
 git add -A && git commit -m "Update Feed"
-
 git push --set-upstream origin main
 
-echo "============="
+echo "==================="
